3d40c181e1a5606a298accadfe25baf7fd9e1476
bhmjiyanibhargav <114737032+bhmjiyanibhargav@users.noreply.github.com>
2023-02-07 14:00:18 +0530

    Add files via upload

---
+++ Untitled54.py
@@ -0,0 +1,74 @@
+
+
+# # question 01    jan/29/2023
+1. who is devloped python programing language?
+# Guido van Rossum
+# When he began implementing Python, Guido van Rossum was also reading the published scripts from “Monty Python's Flying Circus”, a BBC comedy series from the 1970s. Van Rossum thought he needed a name that was short, unique, and slightly mysterious, so he decided to call the language Python.
+
+# # question 02    jan / 29 /2023 
+
+# 2.which type of programing dose suport python?
+Python is a multi-paradigm programming language. Object-oriented programming and structured programming are fully supported, and many of their features support functional programming and aspect-oriented programming (including metaprogramming and metaobjects).
+# # question 03     jan/29/2023
+3.is python case sensitive when dealing with identifier?
+# Yes, Python is a case-sensitive language, i.e., it treats uppercase and lowercase characters differently. This applies to identifiers too. You must avoid using the same name with different cases while naming identifiers.
+
+# # question 04
+4.what is the correct extension of the python file?
+# .py
+# Python is a widely used programming or coding language used for different purposes. The extensions for Python files are - . py, . pyi
+
+# # question 5 
+5.is python code compiled or interpreted?Python is an interpreted language, which means the source code of a Python program is converted into bytecode that is then executed by the Python virtual machine. Python is different from major compiled languages, such as C and C + +, as Python code is not required to be built and linked like code for these languages.
+# # question 6
+6.name a few block of code used to define in python?Python blocks of code
+A Python program is constructed from code blocks. A block is a piece of Python program text that is executed as a unit. The following are blocks: a module, a function body, and a class definition. Each command typed interactively is a block.
+# # question 07
+# 
+7.state a catheter used to give single line comments in python?In Python, any statement which begins with a hash (#) symbol is a comment. So, any code you write after the hash (#) symbol doesn't get executed.
+# In[5]:
+
+
+#for example 
+# my name is bhargav bhimjiyani
+
+
+# # question 08
+8.mantion functions which can help us to find the virsion of python that we are currantly working on?1. Which of the following functions can help us to find the version of python that we are currently working on? Explanation: The function sys. version can help us to find the version of python that we are currently working on.
+# # question 09
+9.python supports the creation of anonymous functions at runtime using a construct called?Python supports to make anonymous functions which are not bound to a name(@ runtime). It uses a construct called "lambda" .
+# # question 10
+10.what dose pip stands for python?preferred installer program
+Alternatively, pip stands for "preferred installer program". Python 2.7. 9 and later (on the python2 series), and Python 3.4 and later include pip (pip3 for Python 3) by default.
+
+# # question 11
+11.mantion a few built in functions in python?bool()	      Returns the boolean value of the specified object
+dict()	      Returns a dictionary (Array)
+complex()	  Returns a complex number
+filter()	  Use a filter function to exclude items in an iterable object
+format()	  Formats a specified value
+input()	      Allowing user input
+int()	      Returns an integer number
+# # question 12
+12.what is the maximum possible length of an identifier in python?79 characters
+Answer: An identifier can have a maximum length of 79 characters in Python. Python is one of the most popular programming languages.
+# # question 13
+13.what are the benefit of using python?
+Data science.
+Scientific and mathematical computing.
+Web development.
+Finance and trading.
+System automation and administration.
+Computer graphics.
+Basic game development.
+Security and penetration testing.
+# # question 14
+14.how is memory managed in python?Memory management in Python involves a private heap containing all Python objects and data structures. The management of this private heap is ensured internally by the Python memory manager.
+# # question 15
+15.how to install python on windows and set path variable?The complete path of python.exe can be added by:
+Right-clicking This PC and going to Properties.
+Clicking on the Advanced system settings in the menu on the left.
+Clicking on the Environment Variables button o​n the bottom right.
+In the System variables section, selecting the Path variable and clicking on Edit.
+# # question 16
+16.is indentation required in python?Indentation refers to the spaces at the beginning of a code line. Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important. Python uses indentation to indicate a block of code.
